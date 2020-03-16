7fe1f2a0058ef83841456878e305aef204c56e62
twan-hub <59781823+twan-hub@users.noreply.github.com>
2020-03-16 11:01:58 -0700

    Add files via upload

---
+++ Launch-Checklist-Form/script.js
@@ -0,0 +1,18 @@
+var pilotName=document.getElementById('pilotName');
+var copilotName=document.getElementById('copilotName');
+var fuelLevel=document.getElementById('fuelLevel');
+var cargoMass=document.getElementById('cargoMass');
+var formSubmit=document.getElementById('formSubmit');
+var faultyItems=document.getElementById('faultyItems');
+
+/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+<h2>Mission Destination</h2>
+<ol>
+   <li>Name: ${pilotName} and ${copilotName}</li>
+   <li>Diameter: ${fuelLevel.value < 10000 ? }</li>
+   <li>Star: ${}</li>
+   <li>Distance from Earth: ${}</li>
+   <li>Number of Moons: ${}</li>
+</ol>
+<img src="${}">
+*/
